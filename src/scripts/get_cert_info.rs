1ce2f67236f74152b5efac5ad4c50b69f25dcbb9
amigin <amigin@gmail.com>
2026-01-06 14:39:05 +0200

    Fixes

--- src/scripts/get_cert_info.rs
+++ src/scripts/get_cert_info.rs
@@ -4,7 +4,7 @@ pub async fn get_cert_info(domain: &str) -> Result<CertificateInfo, String> {
     let cert = super::get_fullchain(domain).await?;
     let private_key = super::get_private_key(domain).await?;
 
-    let cert = my_tls::cert_info::SslCertificate::new(cert.into_bytes(), private_key.into_bytes())?;
+    let cert = my_tls::cert_info::SslCertificate::new(private_key.into_bytes(), cert.into_bytes())?;
 
     Ok(cert.get_cert_info())
 }
